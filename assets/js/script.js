6505b9580f28d38a69b9e5d80d688c123f84e8ee
Dan Erekson <dan.erekson@gmail.com>
2022-07-24 15:05:11 -0400

    basic 8 character PW generation working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Generate password function
+function generatePassword() {
+  var length = 8,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        retVal = "";
+    for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+    }
+    return retVal;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
