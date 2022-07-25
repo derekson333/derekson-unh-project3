e74ef0e453c4b688d19575c74539736a4d0d9fa9
Dan Erekson <dan.erekson@gmail.com>
2022-07-25 16:45:08 -0400

    Functional version of password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,14 +4,39 @@ var generateBtn = document.querySelector("#generate");
 // Generate password function
 function generatePassword() {
   var length = prompt("How long should the password be?"),
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+        charset = "",
         retVal = "";
     if (length < 8 || length > 128) {
       var length = prompt("How long should the password be? (Select a number between 8 and 128)")
     }
+    
+    // Function to use confirm dialogs to choose the type of characters
+    function selectCharset() {
+      if (confirm("Do you want to include lowercase letters?")) {
+      charset = charset + "abcdefghijklmnopqrstuvwxyz"
+      }
+      if (confirm("Do you want to include uppercase letters?")) {
+        charset = charset + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      }
+      if (confirm("Do you want to include numbers?")) {
+        charset = charset + "0123456789"
+      }
+      if (confirm("Do you want to include special characters?")) {
+        charset = charset + "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+      }
+      if (charset === "") {
+        alert("You must choose at lesat one type of characters")
+        selectCharset()
+      }
+    }
+    
+    // Calls the character type selection function
+    selectCharset()
+
     for (var i = 0, n = charset.length; i < length; ++i) {
         retVal += charset.charAt(Math.floor(Math.random() * n));
     }
+  
     return retVal;
 }
 
